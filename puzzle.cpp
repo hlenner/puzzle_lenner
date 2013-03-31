263a5a353223b6353a9b2a21a76fd0511abf72da
Haley Lenner <lenner@usc.edu>
2013-03-30 21:02:21 -0700

    puzzle gameplay implementation close #2

--- puzzle.cpp
+++ puzzle.cpp
@@ -4,6 +4,7 @@
 #include "board.h"
 #include "puzzle_heur.h"
 #include "puzzle_solver.h"
+#include <math.h>
 
 using namespace std;
 
@@ -21,14 +22,28 @@ int main(int argc, char *argv[])
   seed = atoi(argv[3]);
 
   Board b(size,initMoves,seed);
+  cout << b;
 
   //**** Implement the gameplay here
-
-
-
-
-
-
+  int y;
+ 
+  while (b.solved()==false){
+  //ManhattanHeuristic *Manhattan = new ManhattanHeuristic();
+  //OutOfPlaceHeuristic *Place = new OutOfPlaceHeuristic();
+  
+  //cout << "Manhattan: " << Manhattan->compute(b.getTiles(), b.getSize()) << endl;
+  //cout << "Out Of Place: " << Place->compute(b.getTiles(), b.getSize()) << endl;
+  
+  cout << "Enter tile number to move or -1 for a cheat:  " << endl;
+  cin >> y;
+    if (y == -1){
+ 
+  }
+  else{
+  b.move(y);
+  }
+cout << b;
+}
 
   return 0;
 }
